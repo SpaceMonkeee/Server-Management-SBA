5aafb653d779e8ee72efc3164de12adb887541b9
SpaceMonkeee <117305862+SpaceMonkeee@users.noreply.github.com>
2022-11-03 17:11:44 -0700

    Add files via upload

---
+++ server/src/test/java/com/VSSB/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VSSB.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
