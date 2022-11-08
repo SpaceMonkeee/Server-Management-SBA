adbd41820f639ec1484870889eb7786a139cefa1
suliaax <arthur.sulian@abbott.com>
2022-11-08 12:43:14 -0800

    more http stuff
    replaced map.of with manual map initialization

--- server/src/main/java/com/VSSB/server/resource/ServerResource.java
+++ server/src/main/java/com/VSSB/server/resource/ServerResource.java
@@ -1,16 +1,27 @@
 package com.VSSB.server.resource;
 
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
-import java.security.Timestamp;
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.VSSB.server.enumeration.Status;
 import com.VSSB.server.model.Response;
+import com.VSSB.server.model.Server;
 import com.VSSB.server.service.implementation.ServerServiceImpl;
 
 import lombok.RequiredArgsConstructor;
@@ -21,14 +32,51 @@ import lombok.RequiredArgsConstructor;
 public class ServerResource {
     private final ServerServiceImpl serverService;
 
+    @GetMapping("/list")
     public ResponseEntity<Response> getServers(){
+        Map<String,Collection<Server>> data = new HashMap<>();
+        data.put("servers", serverService.list(30));
         return ResponseEntity.ok(
                 Response.builder()
                         .timeStamp(LocalDateTime.now())
-                        .data(Map.of("servers", serverService.list(30)))
+                        .data(data)
+                        //.data(Map.of("servers", serverService.list(30)))
                         .message("Servers retrieved")
                         .status(OK)
                         .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException{
+        Server server = serverService.ping(ipAddress);
+        Map<String,Server> data = new HashMap<>();
+        data.put("server", server);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(data)
+                        //.data(Map.of("servers", serverService.list(30)))
+                        .message(server.getStatus() == Status.SERVER_UP ? "Ping success" : "Ping failed")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
+        Map<String,Server> data = new HashMap<>();
+        data.put("server", serverService.create(server));
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(data)
+                        //.data(Map.of("servers", serverService.list(30)))
+                        .message("Server created")
+                        .status(CREATED)
+                        .statusCode(CREATED.value())
                         .build()
         );
     }
